f416b0984abd0cb40b0b1b3c51bbc663c12727eb
qosua <simonlvovqc@gmail.com>
2026-03-25 22:56:45 +0300

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <QApplication>
 #include <QStyleFactory>
 
+#include "gui/window-manager.h"
 
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("windows11"));
 
-
+    WindowManager winManager;
+    winManager.show();
 
     return QApplication::exec();
 }
